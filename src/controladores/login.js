b07dac19e8836708401f936f89b2f0e2b38e6dcf
caioe <caioevandro0@gmail.com>
2023-10-04 17:33:42 -0300

    exercicio finalizado

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -1,5 +1,36 @@
+const transportador = require("../email")
+const compiladorHtml = require("../utils/compiladorHTML")
+
+
+const usuario = {
+    nome: "Caio",
+    email: "caioevandro81@gmail.com",
+    senha: "123abc"
+
+}
 
 const login = async (req, res) => {
+    const { email, senha } = req.body
+
+    if (usuario.email != email) {
+        return res.status(400).json({mensagem: "Credenciais invalidas"})
+    }
+    if (usuario.senha != senha) {
+        return res.status(400).json({mensagem: "Credenciais invalidas"})
+    }
+    //fazer envio de email
+
+    
+    const html = await compiladorHtml("./src/templates/login.html", {
+        nomeusuario: usuario.nome
+    })
+
+    transportador.sendMail({
+        from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+        to: `${usuario.nome} <${usuario.email}>`,
+        subject: "tentaiva de login",
+        html,
+    })
     return res.json({mensagem: "login concluido"})
 }
 
